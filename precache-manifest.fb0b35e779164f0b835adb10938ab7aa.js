7351644ad314552def2970ad0f29b63657f40b44
HARSH JAIN <jainharsh270@gmail.com>
2020-04-08 12:37:25 +0530

    Updates

---
+++ precache-manifest.fb0b35e779164f0b835adb10938ab7aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da897d2fd1babfdbf006aebd4acf511e",
+    "url": "/Roll-IT/index.html"
+  },
+  {
+    "revision": "f9686c0585393f90afa1",
+    "url": "/Roll-IT/static/css/main.c87532db.chunk.css"
+  },
+  {
+    "revision": "99e92f52a9bd728b9131",
+    "url": "/Roll-IT/static/js/2.ad652cfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Roll-IT/static/js/2.ad652cfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9686c0585393f90afa1",
+    "url": "/Roll-IT/static/js/main.711c8be0.chunk.js"
+  },
+  {
+    "revision": "e1d23720d0e44754a6fa",
+    "url": "/Roll-IT/static/js/runtime-main.f500655a.js"
+  }
+]);
